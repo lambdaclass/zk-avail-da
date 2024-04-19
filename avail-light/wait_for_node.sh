e5b6c31124e3ff0ff3abc08e72c9f9b7b1e45f2e
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-04-19 11:18:29 +0200

    Redirect local traffic in light node

--- avail-light/wait_for_node.sh
+++ avail-light/wait_for_node.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+sh -c "socat TCP-LISTEN:9944,fork TCP:avail-da-node:9944 &"
+sh -c "socat TCP-LISTEN:39000,fork TCP:avail-light-bootstrap:39000 &"
 
 while ! curl -s avail-da-node:9944 > /dev/null; do
     echo "Waiting for avail-da-node to start..."
-    sleep 10
+    sleep 1
 done
 echo "avail-da-node is now running."
 sh -c "cargo run --release -- --network local -c config.yaml --clean"
