d1e52cb34e28e819f54bc3c3a1924e6ee03eda7d
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-05-21 12:00:03 +0200

    Debug and generateAccount

--- validium/submitData.js
+++ validium/submitData.js
@@ -1,8 +1,43 @@
 import { Keyring } from "@polkadot/api";
+import {
+    mnemonicGenerate,
+    mnemonicToMiniSecret,
+    mnemonicValidate,
+    ed25519PairFromSeed,
+} from "@polkadot/util-crypto";
 import * as dotenv from "dotenv";
 import { createApi, sendTx } from "./common.js";
 
 dotenv.config();
+
+async function generateAccount() {
+    // const mnemonic = mnemonicGenerate();
+    // console.log(`Generated Mnemonic: ${mnemonic}`);
+    // const keyring = new Keyring({ type: "sr25519" });
+    // const account = keyring.addFromUri(mnemonic);
+    // console.log(`Account Address: ${account.address}`);
+
+    // Create mnemonic string for Alice using BIP39
+    const mnemonicAlice = mnemonicGenerate();
+
+    console.log(`Generated mnemonic: ${mnemonicAlice}`);
+
+    // Validate the mnemonic string that was generated
+    const isValidMnemonic = mnemonicValidate(mnemonicAlice);
+
+    console.log(`isValidMnemonic: ${isValidMnemonic}`);
+
+    // Create valid Substrate-compatible seed from mnemonic
+    const seedAlice = mnemonicToMiniSecret(mnemonicAlice);
+
+    // Generate new public/secret keypair for Alice from the supplied seed
+    const { publicKey, secretKey } = ed25519PairFromSeed(seedAlice);
+    const keyring = new Keyring({ type: "sr25519" });
+    const account = keyring.addFromUri(mnemonicAlice);
+    console.log(`Account Address: ${account.address}`);
+
+    return account;
+}
 
 /**
  * Submitting data to Avail as a transaction.
@@ -13,17 +48,22 @@ dotenv.config();
  * @returns {Promise<unknown>}
  */
 async function submitData(availApi, data, account) {
-    console.log("Submitting Data...");
-    // Retrieve the chain name
-    const chain = await availApi.rpc.system.chain();
-    console.log(`rpc chain = ${chain}`);
-    // Retrieve the latest header
-    const lastHeader = await availApi.rpc.chain.getHeader();
+    try {
+        console.log("Submitting Data...");
+        const chain = await availApi.rpc.system.chain();
+        const lastHeader = await availApi.rpc.chain.getHeader();
+        console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
 
-    // Log the information
-    console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
-    let submit = await availApi.tx.dataAvailability.submitData(data);
-    return await sendTx(availApi, account, submit);
+        let submit = await availApi.tx.dataAvailability.submitData(data);
+        console.log(`Submit = ${submit}`);
+
+        console.log("Send Tx...");
+        const result = await sendTx(availApi, account, submit);
+        return result;
+    } catch (error) {
+        console.error("Error in submitData:", error);
+        throw error;
+    }
 }
 
 /**
@@ -65,29 +105,34 @@ async function getDataRoot(availApi, blockHash) {
  * Since the bridge is optimistic, it is necessary to wait for 30 minutes before the data root is available on Ethereum.
  */
 (async function dataRootDispatch() {
-    const availApi = await createApi(process.env.AVAIL_RPC);
-    const keyring = new Keyring({
-        type: "sr25519",
-    });
-    const account = keyring.addFromMnemonic(process.env.SURI);
-    console.log("Submitting data to Avail...");
+    try {
+        const availApi = await createApi(process.env.AVAIL_RPC);
+        // const keyring = new Keyring({
+        //     type: "sr25519",
+        // });
+        // const account = keyring.addFromMnemonic(process.env.SURI);
+        // console.log("Submitting data to Avail...");
+        const account = await generateAccount();
 
-    let result = await submitData(availApi, "0", account);
-    console.log("Data Submitted!");
+        console.log(`Account address: ${account.address}`);
+        console.log(`Account details: ${JSON.stringify(account)}`);
 
-    const txIndex = JSON.parse(result.events[0].phase).applyExtrinsic;
-    const blockHash = result.status.asInBlock;
-    console.log(
-        `Transaction: ${result.txHash}. Block hash: ${blockHash}. Transaction index: ${txIndex}.`,
-    );
+        console.log("Submitting data to Avail...");
+        let result = await submitData(availApi, "0", account);
 
-    console.log("Triggering Home...");
-    result = await dispatchDataRoot(availApi, blockHash, account);
-    console.log(`Sent txn on Avail. Txn Hash: ${result.txHash}.`);
-    let [root, blockNum] = await getDataRoot(availApi, blockHash);
-    console.log("Data Root:" + root + " and Block number: " + blockNum);
+        console.log("Data Submitted!");
+        const txIndex = result.events[0].phase.toHuman().applyExtrinsic;
+        const blockHash = result.status.asInBlock;
 
-    await availApi.disconnect();
+        console.log(
+            `Transaction: ${result.txHash}. Block hash: ${blockHash}. Transaction index: ${txIndex}.`,
+        );
+
+        await availApi.disconnect();
+    } catch (error) {
+        console.error("Error in dataRootDispatch:", error);
+        process.exit(1);
+    }
 })()
     .then(() => {
         console.log("Done");
