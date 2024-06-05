545dc8dc91f204a2048770e3784a40ef4fb473f9
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-06-05 14:34:01 +0200

    Make it work

--- tools/web_scraper/web_scraper.py
+++ tools/web_scraper/web_scraper.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
@@ -13,12 +15,24 @@ ADDRESS_INPUT_TAG = "address"
 BUTTON_TEXT = "Get AVAIL"
 address = os.getenv('ADDRESS')
 
-driver = webdriver.Chrome()
-driver.maximize_window()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
-wait = WebDriverWait(driver, 10)
-wait.until(EC.visibility_of_element_located((By.ID, ADDRESS_INPUT_TAG))).send_keys(address + Keys.ENTER)
-wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[text()='{BUTTON_TEXT}']"))).click()
-driver.implicitly_wait(10)
-driver.quit()
+while (True):
+    try:
+        wait = WebDriverWait(driver, 10)
+        address_input = wait.until(EC.visibility_of_element_located((By.ID, ADDRESS_INPUT_TAG)))
+        address_input.send_keys(address)
+
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[text()='{BUTTON_TEXT}']")))
+        button.click()
+        print(f"Successfully Get AVAIL!")
+        time.sleep(86400) # Wait a day
+    finally:
+        driver.quit()
