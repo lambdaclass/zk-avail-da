79648efa6dfd3029d1a00b1c5fcb54375f7096c3
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-06-07 15:30:10 +0200

    Fmt

--- validium/get_pubdata.ts
+++ validium/get_pubdata.ts
@@ -6,21 +6,21 @@ const env = await load();
 const AVAIL_RPC = env["AVAIL_RPC"];
 
 function writeJson(path: string, data: object): string {
-    try {
-      Deno.writeTextFileSync(path, JSON.stringify(data));
-      return "Written to " + path;
-    } catch (e) {
-      return e.message;
-    }
+  try {
+    Deno.writeTextFileSync(path, JSON.stringify(data));
+    return "Written to " + path;
+  } catch (e) {
+    return e.message;
+  }
 }
 
 export async function getPubdata(hash: string) {
-    const availApi = await initializeAvailApi(AVAIL_RPC);
-    const rows = await availApi.rpc.kate.queryRows([0], hash);
-    const data = rows.toHuman();
-    const fileName = `./pubdata/${hash}.json`;
-    console.log(writeJson(fileName, data));
-    await availApi.disconnect();
+  const availApi = await initializeAvailApi(AVAIL_RPC);
+  const rows = await availApi.rpc.kate.queryRows([0], hash);
+  const data = rows.toHuman();
+  const fileName = `./pubdata/${hash}.json`;
+  console.log(writeJson(fileName, data));
+  await availApi.disconnect();
 }
 
 getPubdata(Deno.args[0]);
